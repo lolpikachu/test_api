b399a95d1b6a94b8d1bca386a9f2d8309284e7cb
Lika <anzhelikadarijchuk@gmail.com>
2020-09-29 15:05:16 +0200

    changed db user

--- api_axample/settings.py
+++ api_axample/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'api_example',
-        'USER': 'postgres',
+        'USER': 'pikachu',
         'PASSWORD': '350350f350350f',
         'HOST': 'localhost',
         'PORT': '',
